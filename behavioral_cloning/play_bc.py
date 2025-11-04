ff639f21dda084bcb8dc063831eaeac72e26187f
gonzatorra <haizea.g@opendeusto.es>
2025-11-04 14:31:48 +0100

    añadido clase

--- behavioral_cloning/play_bc.py
+++ behavioral_cloning/play_bc.py
@@ -13,16 +13,27 @@ model.eval()
 
 obs, _ = env.reset()
 
-while True:
-    #Normalize the observation
-    obs_input = torch.tensor(obs/255.0, dtype=torch.float32).unsqueeze(0)
-    with torch.no_grad():
-        #Get the result, the vector that the network predicts --> [steering, gas, brake]
-        action = model(obs_input).squeeze(0).numpy()
-    #Put that action into the environment
-    obs, reward, terminated, truncated, info = env.step(action)
+return_per_episode = []
+for _ in range(10):
+    total_return = 0
+    while True:
+        #Normalize the observation
+        obs_input = torch.tensor(obs/255.0, dtype=torch.float32).unsqueeze(0)
+        with torch.no_grad():
+            #Get the result, the vector that the network predicts --> [steering, gas, brake]
+            action = model(obs_input).squeeze(0).numpy()
+        #Put that action into the environment
+        obs, reward, terminated, truncated, info = env.step(action)
+        total_return += reward
 
-    if terminated or truncated:
-        obs, _ = env.reset()
+        if terminated or truncated:
+            obs, _ = env.reset()
+            print("TOTAL EPISODE REWARD: ", total_return)
+            print("RESET")
+            return_per_episode.append(total_return)
+            break
 
+print("TOTAL RETURN: ", total_return)
+print("REWARD AVERAGE: ", np.mean(return_per_episode))
+print("STD: ", np.std(return_per_episode))
 env.close()
